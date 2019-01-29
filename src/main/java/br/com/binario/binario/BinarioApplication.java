4ba7327e86c7d7de8b8df09350db36b76d71a8fa
anderson-figueiredo <figueiredo.anderson@rocketmail.com>
2019-01-29 18:32:38 -0200

    Inicio

---
+++ src/main/java/br/com/binario/binario/BinarioApplication.java
@@ -0,0 +1,14 @@
+package br.com.binario.binario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinarioApplication.class, args);
+	}
+
+}
+
